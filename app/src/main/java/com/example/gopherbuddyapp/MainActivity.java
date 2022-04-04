30982023d7189bf3a25e98b8d9a2477f1a70b1bb
Alex Overman <alex.jordan.ayres@gmail.com>
2022-04-03 19:34:43 -0500

    Removed notifications tab

--- app/src/main/java/com/example/gopherbuddyapp/MainActivity.java
+++ app/src/main/java/com/example/gopherbuddyapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_findspace, R.id.navigation_notifications, R.id.navigation_myprofile)
+                R.id.navigation_home, R.id.navigation_findspace, R.id.navigation_buddyprofile, R.id.navigation_myprofile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
