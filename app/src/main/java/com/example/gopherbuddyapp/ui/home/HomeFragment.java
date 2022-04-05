74d6f08390c8d662cb79ef922ff311e5e9ba65d7
broad093 <broad093@umn.edu>
2022-04-05 05:41:21 -0500

    find space design

--- app/src/main/java/com/example/gopherbuddyapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gopherbuddyapp/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.gopherbuddyapp.ui.home;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+<<<<<<< Updated upstream
+=======
+
+
+import com.example.gopherbuddyapp.MainActivity;
+import com.example.gopherbuddyapp.R;
+>>>>>>> Stashed changes
 import com.example.gopherbuddyapp.databinding.FragmentHomeBinding;
+import com.example.gopherbuddyapp.ui.buddyselect.BuddySelectFragment;
+import com.example.gopherbuddyapp.ui.myprofile.ExternalPlatformFragment;
+import com.example.gopherbuddyapp.ui.myprofile.ProfileSettingsFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -18,14 +30,30 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+<<<<<<< Updated upstream
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+=======
+        new CountDownTimer(3000, 100)  {
+            public void onTick(long millisUnitlFinished) {
+
+            }
+            public void onFinish() {
+                BuddySelectFragment buddySelectFragment = new BuddySelectFragment();
+                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, buddySelectFragment);
+                fragmentTransaction.commit();
+            }
+        }.start();
+
+>>>>>>> Stashed changes
         return root;
     }
 
